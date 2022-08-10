efb9791aa31f29f867114124c5c32b06b13ae74c
JLivlos <mistlivlos@gmail.com>
2022-08-10 16:42:16 +0300

    make solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = arguments[0];
+        this.max = arguments[1];
     }
 
     guess() {
-
+        this.result = Math.ceil((this.max - this.min) / 2) + this.min;;
+        return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result;
     }
 }
 
